d5122d24c4256f2c22eb7f68a9dc7bcb070812ce
Lee <ssing365@naver.com>
2024-10-15 18:09:45 +0900

    learn useEffect

--- react_project/src/App.js
+++ react_project/src/App.js
@@ -1,17 +1,43 @@
-import React, { useState } from "react";
-import Button from "./Button";
-import styles from "./App.module.css";
+import React, { useState, useEffect } from "react";
 
 const App = () => {
   const [counter, setCounter] = useState(0);
+  const [keyword, setKeyword] = useState("");
+
   function onClick() {
     setCounter((prev) => prev + 1);
   }
+  const onChange = (event) => {
+    setKeyword(event.target.value);
+  };
+
+  useEffect(() => {
+    console.log("this code run only once. call an API");
+  }, []);
+  useEffect(() => {
+    if (keyword !== "" && keyword.length > 5) { //이건 처음 렌더링할 때도 실행되지 않음.
+      console.log(
+        `this code run when keyword changes and length of keyword is more than 5. search for ${keyword}`
+      );
+    }
+  }, [keyword]);
+  useEffect(() => {
+    console.log(`this code run when counter changes`);
+  }, [counter]);
+  useEffect(() => {
+    console.log(`this code run when counter and keyword changes`);
+  }, [counter, keyword]);
+
   return (
     <>
-      <h1 className={styles.potato}>Welcome!</h1>
+      <input
+        onChange={onChange}
+        value={keyword}
+        type="text"
+        placeholder="Search here..."
+      ></input>
       <h1>{counter}</h1>
-      <Button onClick={onClick} text="up!" />
+      <button onClick={onClick}>up!</button>
     </>
   );
 };
