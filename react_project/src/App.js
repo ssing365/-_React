29be7b361f039811581d0774353dee25c5e61d13
Hosung <ssing365@naver.com>
2024-10-15 23:32:35 +0900

    useEffect 연습 + cleanup function

--- react_project/src/App.js
+++ react_project/src/App.js
@@ -1,43 +1,22 @@
 import React, { useState, useEffect } from "react";
 
 const App = () => {
-  const [counter, setCounter] = useState(0);
-  const [keyword, setKeyword] = useState("");
+  const [showing, setShowing] = useState(true);
 
-  function onClick() {
-    setCounter((prev) => prev + 1);
+  const onClick = () => setShowing((prev) => !prev);
+
+  const Hello = () => {
+    useEffect(() => {
+      console.log('hi :)')
+      return () => console.log('bye :(') //clean-up fn
+    }, [])
+    return <h1>Hello</h1>
   }
-  const onChange = (event) => {
-    setKeyword(event.target.value);
-  };
-
-  useEffect(() => {
-    console.log("this code run only once. call an API");
-  }, []);
-  useEffect(() => {
-    if (keyword !== "" && keyword.length > 5) { //이건 처음 렌더링할 때도 실행되지 않음.
-      console.log(
-        `this code run when keyword changes and length of keyword is more than 5. search for ${keyword}`
-      );
-    }
-  }, [keyword]);
-  useEffect(() => {
-    console.log(`this code run when counter changes`);
-  }, [counter]);
-  useEffect(() => {
-    console.log(`this code run when counter and keyword changes`);
-  }, [counter, keyword]);
 
   return (
     <>
-      <input
-        onChange={onChange}
-        value={keyword}
-        type="text"
-        placeholder="Search here..."
-      ></input>
-      <h1>{counter}</h1>
-      <button onClick={onClick}>up!</button>
+      <button onClick={onClick}>{showing ? 'show' : 'hide'}</button>
+      {showing ? <Hello /> : null}
     </>
   );
 };
