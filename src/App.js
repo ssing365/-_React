23349dc2574b0f76bf9d9dacbf07787ad8cf6c1b
Lee <ssing365@naver.com>
2024-10-16 12:30:43 +0900

    component, props, state

--- src/App.js
+++ src/App.js
@@ -1,18 +1,42 @@
-import React, { useState } from 'react';
+import {useState} from 'react';
+import PropTypes from 'prop-types';
 
-const App = () => {
-    const [counter, setCounter] = React.useState(0);
-    
-    const onClick = () => {
-      setCounter((current) => current+10)
-    }
-  
-    return(
-      <div>
-        <h3>{counter}번 클릭되었습니다.</h3>
-        <button onClick={onClick}>click me</button>
-      </div>
-    )
-  };
+function App({name, children}) {
+
+  const [color, setColor] = useState('black');
+
+  return (
+    <>
+    <div>
+      <header>헤더</header>
+      <main>{children}</main>
+      <div>안녕하세요, 제 이름은{name}입니다.</div>
+      <footer>푸터</footer>
+    </div>
+
+    {/* color btn */}
+    <div>
+    <h1 style={{ color }}>{'뭐여뭐여'}</h1>
+    <button style={{ color: 'red' }} onClick={() => setColor('red')}>
+      빨간색
+    </button>
+    <button style={{ color: 'green' }} onClick={() => setColor('green')}>
+      초록색
+    </button>
+    <button style={{ color: 'blue' }} onClick={() => setColor('blue')}>
+      파란색
+    </button>
+  </div>
+  </>
+  );
+}
+
+App.defaultProps = {
+  name : '홍길동'
+}
+
+App.propTypes = {
+  name : PropTypes.string
+}
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,5 +1,12 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import ReactDOM from 'react-dom/client';
 import App from './App';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+    <App name={' dodo '}>
+      <h1>안녕하세요!</h1>
+      <p>이것은 메인 콘텐츠입니다.</p>
+    </App>
+);
+
