55894252a34c4903e60f8eaf02c2ade116bd93a7
Hosung <ssing365@naver.com>
2024-10-17 00:45:19 +0900

    Coin Tracker complete

--- react_project/src/App.js
+++ react_project/src/App.js
@@ -1,28 +1,52 @@
 import React, { useEffect, useState } from "react";
+import styles from './Select.module.css';
 
 const App = () => {
   const [loding, setLoding] = useState(true);
-  const [coins, setCoin] = useState([]);
+  const [coins, setCoins] = useState([]);
+  const [dollar, setDollar] = useState(0);
+  const [selectedCoin, setSelectedCoin] = useState(null);
+
+  const onChange = (event) => setDollar(event.target.value);
+  const onChangeOption = (event) => {
+    console.log(event.target.value); 
+    const coin = coins.find(c => c.id === event.target.value);
+    setSelectedCoin(coin);
+  };
+
   const API = "https://api.coinpaprika.com/v1/tickers";
   useEffect(() => {
     fetch(API).then((response) =>
       response.json().then((json) => {
         setLoding(false);
-        setCoin(json);
-        console.dir(json[0])
+        setCoins(json);
+        console.dir(json[0]);
+        setSelectedCoin(json[0]);
       })
     );
   }, []);
   return (
     <div>
-      <h1>The Coins! {loding ? null : `(${coins.length})` }</h1>
+      <h1>The Coins!</h1>
       <hr />
-      <strong>{loding ? "Loading..." : <select>
-        {coins.map((coin, index) => 
-          <option key={index}> {coin.name} ({coin.symbol}) : ${(coin.quotes.USD.price)} </option>
+      <strong>{loding ? "Loading..." : <select onChange={onChangeOption} value={selectedCoin ? selectedCoin.id : ""} className={styles.select}>
+        {coins.map((coin) =>
+          <option key={coin.id} value={coin.id}> {coin.name} ({coin.symbol}) : ${(coin.quotes.USD.price)} </option>
         )}
       </select>}</strong>
+      <b>{loding ? null : `(Total Number of Coins : ${coins.length})`}</b>
+
+      <br /><br /><br /><br />
+
       {/*코드 챌린지 : 사용자에게 달러 입력 받고 비트코인 얼만큼 살 수 있는지 알려주기*/}
+      <form>
+        <label>Dollars you have : $ </label>
+        <input value={dollar} onChange={onChange} type="number" placeholder="dollors" />
+        <br /><br />
+
+        <label>{`How Much You Can Buy ${selectedCoin ? selectedCoin.name : 'this coin'} with Your Dollars : `}</label>
+        <input value={selectedCoin ? (dollar / selectedCoin.quotes.USD.price).toFixed(6) : 0} type="number" placeholder="0" disabled />
+      </form>
     </div>
   );
 };
