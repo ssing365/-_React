c43b7f3598aaffc66b13d6b82e863aeca10b76d7
Lee <ssing365@naver.com>
2024-10-16 12:59:46 +0900

    dd

--- react_project/src/App.js
+++ react_project/src/App.js
@@ -1,24 +1,11 @@
-import React, { useState, useEffect } from "react";
+import React from 'react';
 
 const App = () => {
-  const [showing, setShowing] = useState(true);
-
-  const onClick = () => setShowing((prev) => !prev);
-
-  const Hello = () => {
-    useEffect(() => {
-      console.log('hi :)')
-      return () => console.log('bye :(') //clean-up fn
-    }, [])
-    return <h1>Hello</h1>
-  }
-
-  return (
-    <>
-      <button onClick={onClick}>{showing ? 'show' : 'hide'}</button>
-      {showing ? <Hello /> : null}
-    </>
-  );
+    return (
+        <div>
+            
+        </div>
+    );
 };
 
 export default App;
