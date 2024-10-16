64debb10a5942eece94ef71fe671aed96a676169
Lee <ssing365@naver.com>
2024-10-16 18:28:33 +0900

    Coin Tracker

--- react_project/src/App.js
+++ react_project/src/App.js
@@ -1,42 +1,28 @@
 import React, { useEffect, useState } from "react";
 
 const App = () => {
-  const [todo, setTodo] = useState("");
-  const [todos, setTodos] = useState([]);
-
-  const onChange = (event) => setTodo(event.target.value);
-  const onSubmit = (event) => {
-    event.preventDefault();
-    if (todo === "") {
-      return;
-    }
-    setTodo("");
-
-    setTodos((currentArray) => [todo, ...currentArray]);
-  };
-
-  console.log(todos.map((item, index) => (
-    <li key={index}>{item}</li>
-  )))
+  const [loding, setLoding] = useState(true);
+  const [coins, setCoin] = useState([]);
+  const API = "https://api.coinpaprika.com/v1/tickers";
   useEffect(() => {
-    console.log(todos);
-  }, [todos]);
-
+    fetch(API).then((response) =>
+      response.json().then((json) => {
+        setLoding(false);
+        setCoin(json);
+        console.dir(json[0])
+      })
+    );
+  }, []);
   return (
     <div>
-      <h1>My To Dos {todos.length}</h1>
-      <form onSubmit={onSubmit}>
-        <input
-          value={todo}
-          onChange={onChange}
-          type="text"
-          placeholder="Write your to do..."
-        />
-        <button>Add To Do</button>
-        {todos.map((item, index) => (
-          <li key={index}>{item}</li> //JSX안에서 JS문법 쓸때 {}열어서 썼는데 그 안에 다시 JSX(html 태그)썼다가 다시 그 안에 {}로 JS문법 씀
-        ))}
-      </form>
+      <h1>The Coins! {loding ? null : `(${coins.length})` }</h1>
+      <hr />
+      <strong>{loding ? "Loading..." : <select>
+        {coins.map((coin, index) => 
+          <option key={index}> {coin.name} ({coin.symbol}) : ${(coin.quotes.USD.price)} </option>
+        )}
+      </select>}</strong>
+      {/*코드 챌린지 : 사용자에게 달러 입력 받고 비트코인 얼만큼 살 수 있는지 알려주기*/}
     </div>
   );
 };
